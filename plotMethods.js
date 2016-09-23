f90375894a0df69c1c5d40df33d2006f4c9a89bf
lk-devstation-12 <ubuntu@lk-devstation-12.(none)>
2016-09-22 20:09:15 -0400

    Changed params for getSeriesData() and filtering data

--- plotMethods.js
+++ plotMethods.js
@@ -1,4 +1,4 @@
-var filterOperators = ['=', '!=', '>', '<', '>=', '<='];
+var MASK_REGEX = "^[x 0-9\a-z\.\:\(\)\=\!\"\'\&\|\<\>\+\*\-\/\%]*$";
 
 function round(num, precision) {
   return precision === 0 ? Math.round(num) : Math.round(num * Math.pow(10, precision)) / Math.pow(10, precision);
@@ -10,7 +10,7 @@ function round(num, precision) {
  * @param resultKey - String
  * @constructor
  */
-var Plot = function(results, resultKey) {
+var PerspectiveTable = function(results, resultKey) {
   this.result = null;
   this.columnNames = [];
   this.columnData = [];
@@ -21,10 +21,14 @@ var Plot = function(results, resultKey) {
     this.result = results;
   }
 
+  this._createData();
+
+  // TODO: extract all other properties we may need and get rid of the original object
+
   return this;
 };
 
-Plot.prototype.getReportName = function() {
+PerspectiveTable.prototype.getReportName = function() {
   var name = "Untitled";
   if (this.result && this.result.ReportName) {
     name = this.result.ReportName;
@@ -32,15 +36,15 @@ Plot.prototype.getReportName = function() {
   return name;
 };
 
-Plot.prototype.getReportSubtitle = function() {
+PerspectiveTable.prototype.getReportSubtitle = function() {
   return this.result.Request.title;
 };
 
-Plot.prototype.getNumColumns = function() {
+PerspectiveTable.prototype.getNumColumns = function() {
   return this.result.ReportResult.Leaves[0].Data.length;
 };
 
-Plot.prototype.getColumnName = function(colInd) {
+PerspectiveTable.prototype.getColumnName = function(colInd) {
 
   var self = this;
 
@@ -76,35 +80,83 @@ Plot.prototype.getColumnName = function(colInd) {
   return this.columnNames[colInd];
 };
 
-Plot.prototype.getColumn = function(colInd, dataOnly) {
-  var self = this;
-
-  function createColumnData() {
-    var numColumns = self.result.ReportResult.Leaves[0].Data.length;
+/**
+ *
+ * @param mask
+ * @private
+ */
+PerspectiveTable.prototype._createData = function(){
+  if (this.columnData.length === 0) {
+    var numColumns = this.result.ReportResult.Leaves[0].Data.length;
 
     // Add names
     for (var i = 0; i < numColumns; ++i) {
-      self.columnData.push({
-        name: self.getColumnName(i),
+      this.columnData.push({
+        name: this.getColumnName(i),
         data: []
       });
     }
 
     // Add values
-    self.result.ReportResult.Leaves.forEach(function(leaf){
-      // Filter out some values
-      if (leaf.Data[1] !== 0 && leaf.Data[2] > 0) {
-        for (var i = 0; i < numColumns; ++i) {
-          self.columnData[i].data.push(leaf.Data[i]);
-        }
+    var self = this;
+    this.result.ReportResult.Leaves.forEach(function(leaf){
+      for (var i = 0; i < numColumns; ++i) {
+        self.columnData[i].data.push(leaf.Data[i]);
       }
     });
   }
+};
 
-  if (this.columnData.length === 0) {
-    createColumnData();
+PerspectiveTable.prototype._isRowIncluded = function(index, mask) {
+  // Check if we have valid filter mask
+  var included = true;
+  var temp = null;
+  if (mask) {
+    if (typeof(mask) !== "string" || !new RegExp(MASK_REGEX, 'i').test(mask)) {
+      temp = new String(mask);
+    }
   }
 
+  if (temp) {
+    for (var i = 0; i < this.columnData.length; ++i) {
+      temp = temp.split("x:" + i).join(this.columnData[i].data[index]);
+    }
+    included = new Function('return ' + temp)();
+  }
+
+  return included;
+
+};
+
+PerspectiveTable.prototype.getColumn = function(colInd, dataOnly) {
+  // var self = this;
+  //
+  // function createColumnData() {
+  //   var numColumns = self.result.ReportResult.Leaves[0].Data.length;
+  //
+  //   // Add names
+  //   for (var i = 0; i < numColumns; ++i) {
+  //     self.columnData.push({
+  //       name: self.getColumnName(i),
+  //       data: []
+  //     });
+  //   }
+  //
+  //   // Add values
+  //   self.result.ReportResult.Leaves.forEach(function(leaf){
+  //     // Filter out some values
+  //     if (leaf.Data[1] !== 0 && leaf.Data[2] > 0) {
+  //       for (var i = 0; i < numColumns; ++i) {
+  //         self.columnData[i].data.push(leaf.Data[i]);
+  //       }
+  //     }
+  //   });
+  // }
+  //
+  // if (this.columnData.length === 0) {
+  //   createColumnData();
+  // }
+  //
   if (dataOnly === undefined) {
     dataOnly = true;
   }
@@ -112,7 +164,7 @@ Plot.prototype.getColumn = function(colInd, dataOnly) {
   return dataOnly ? this.columnData[colInd].data : this.columnData[colInd];
 };
 
-Plot.prototype.getScatterChartData = function(xCol, yCol, tipCol) {
+PerspectiveTable.prototype.getScatterChartData = function(xCol, yCol, tipCol) {
   console.log('getting Scatter Chart Data');
   var data = [];
   var columnX = this.getColumn(xCol);
@@ -129,7 +181,7 @@ Plot.prototype.getScatterChartData = function(xCol, yCol, tipCol) {
   return data;
 },
 
-Plot.prototype.getBubbleChartData = function(xCol, yCol, sizeCol, nameCol) {
+PerspectiveTable.prototype.getBubbleChartData = function(xCol, yCol, sizeCol, nameCol) {
   console.log('getting Bubble Chart Data');
   var data = [];
   var columnX = this.getColumn(xCol);
@@ -152,94 +204,104 @@ Plot.prototype.getBubbleChartData = function(xCol, yCol, sizeCol, nameCol) {
  * Returns the array of data in the form acceptable for series.data in highchart object
  *
  * @param {Object} result - object to retrive data from
- * @param {String} colSpec - associates result columns with the keys in highchart series.data object , e.g. "x:1,y:2,name:0"
- * @param {String} filterSpec - optional filter specs in string form, e.g. "x>0,y<=5". Should use the same keys as colSpec
- * @return (Array of Objects} [{x: 25}, y: 30},...]
+ * @param {object} colList - associates result columns with the keys in highchart series.data object , e.g. {x:1,y:2,name:0}
+ * @param {String} mask - optional filter mask in string form, e.g. "x:1 > 0 && x:2 <= 5 ...".
+ * @return (Array of Objects} [{x: 25, y: 30},...]
  */
-Plot.prototype.getSeriesData = function(colSpec, filterSpec) {
+PerspectiveTable.prototype.getSeriesData = function(colList, mask) {
   console.log('getting Series Data');
   var data = [];
   var columns = [];
-  var colInfo = colSpec.split(',');
-  var i, j;
-  for (i = 0; i < colInfo.length; ++i) {
-    var info = colInfo[i].split(':');
-    var key = info[0].trim();
-    var colInd = parseInt(info[1].trim());
+
+  for (var key in colList) {
     columns.push({
-      data: this.getColumn(colInd),
+      data: this.getColumn(colList[key]),
       key: key
     });
   }
+  // var colInfo = colSpec.split(',');
+  // var i, j;
+  // for (i = 0; i < colInfo.length; ++i) {
+  //   var info = colInfo[i].split(':');
+  //   var key = info[0].trim();
+  //   var colInd = parseInt(info[1].trim());
+  //   columns.push({
+  //     data: this.getColumn(colInd),
+  //     key: key
+  //   });
+  // }
 
-  var filters = [];
-  if (filterSpec) {
-    var strFilters = filterSpec.split(',');
-    for (i = 0; i < strFilters.length; ++i) {
-      var filter = null;
-      for (j = 0; !filter && j < filterOperators.length; ++j) {
-        var pos = strFilters[i].search(filterOperators[j]);
-        // Can use >0 because operator should be preceded with column index
-        if (pos > 0) {
-          filter = {
-            key: strFilters[i].substr(0, pos).trim(),
-            operator: filterOperators[j],
-            value: parseInt(strFilters[i].substr(pos + filterOperators[j].length).trim())
-          };
-
-          if (filter.key.length > 0 && filter.value !== NaN) {
-            filters.push(filter);
-          }
-        }
-      }
-    }
-  }
+  // var filters = [];
+  // if (filterSpec) {
+  //   var strFilters = filterSpec.split(',');
+  //   for (i = 0; i < strFilters.length; ++i) {
+  //     var filter = null;
+  //     for (j = 0; !filter && j < filterOperators.length; ++j) {
+  //       var pos = strFilters[i].search(filterOperators[j]);
+  //       // Can use >0 because operator should be preceded with column index
+  //       if (pos > 0) {
+  //         filter = {
+  //           key: strFilters[i].substr(0, pos).trim(),
+  //           operator: filterOperators[j],
+  //           value: parseInt(strFilters[i].substr(pos + filterOperators[j].length).trim())
+  //         };
+  //
+  //         if (filter.key.length > 0 && filter.value !== NaN) {
+  //           filters.push(filter);
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
 
   // Assuming data for each column have the same length. If we have use case for a different need to implement some checkings
   var dataSize = columns[0].data.length;
 
   for (i = 0; i < dataSize; ++i) {
-    var elem = {};
-    for (j = 0; j < columns.length; ++j) {
-      elem[columns[j].key] = columns[j].data[i];
-    }
-
-    var add = true;
-    if (filters.length > 0) {
-      // Check whether or not we should filter data out and turn flag if yes
-      for (j = 0; add && j < filters.length; ++j) {
-        var key = filters[j].key;
-        if (elem.hasOwnProperty(key)) {
-          var valueToExamine = elem[key];
-          var filterValue = filters[j].value;
-
-          switch (filters[j].operator) {
-            case '=':
-              add = valueToExamine === filterValue;
-              break;
-            case '!=':
-              add = valueToExamine !== filterValue;
-              break;
-            case '<':
-              add = valueToExamine < filterValue;
-              break;
-            case '>':
-              add = valueToExamine > filterValue;
-              break;
-            case '<=':
-              add = valueToExamine <= filterValue;
-              break;
-            case '>=':
-              add = valueToExamine >= filterValue;
-              break;
-          }
-        }
+    if (this._isRowIncluded(i, mask)) {
+      var point = {};
+      for (j = 0; j < columns.length; ++j) {
+        point[columns[j].key] = columns[j].data[i];
       }
     }
+    data.push(point);
 
-    if (add) {
-      data.push(elem);
-    }
+    // var add = true;
+    // if (filters.length > 0) {
+    //   // Check whether or not we should filter data out and turn flag if yes
+    //   for (j = 0; add && j < filters.length; ++j) {
+    //     var key = filters[j].key;
+    //     if (elem.hasOwnProperty(key)) {
+    //       var valueToExamine = elem[key];
+    //       var filterValue = filters[j].value;
+    //
+    //       switch (filters[j].operator) {
+    //         case '=':
+    //           add = valueToExamine === filterValue;
+    //           break;
+    //         case '!=':
+    //           add = valueToExamine !== filterValue;
+    //           break;
+    //         case '<':
+    //           add = valueToExamine < filterValue;
+    //           break;
+    //         case '>':
+    //           add = valueToExamine > filterValue;
+    //           break;
+    //         case '<=':
+    //           add = valueToExamine <= filterValue;
+    //           break;
+    //         case '>=':
+    //           add = valueToExamine >= filterValue;
+    //           break;
+    //       }
+    //     }
+    //   }
+    // }
+
+    // if (add) {
+    //   data.push(elem);
+    // }
   }
 
   return data;
